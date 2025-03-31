1a0bb721d73e83d3393ff8662b477bff21ce71cc
pescarda007 <pescarda007@gmail.com>
2025-03-31 20:09:16 +0200

    Update 1

--- app.py
+++ app.py
@@ -10,47 +10,39 @@ model=joblib.load(ruta)
 
 
 def main():
-    st.title("Formulario de Datos Personales")
+    st.title("Comprueba tu estado corporal")
     
     # Entrada de datos numéricos
-    edad = st.number_input("Edad", min_value=0, max_value=120, step=1)
-    altura = st.number_input("Altura (cm)", min_value=50, max_value=250, step=1)
-    peso = st.number_input("Peso (kg)", min_value=10, max_value=300, step=1)
-    
-    # Checkbox para historial familiar
-    historial_familiar = st.checkbox("Historial Familiar de Enfermedades")
+    edad = st.number_input("Edad", min_value=1, max_value=120, step=1)
+    altura = st.number_input("Altura (m)", value=0.0, step=0.1, format="%.2f")
+    peso = st.number_input("Peso (kg)", value=0.0, step=0.1, format="%.2f")
     
     # Selectbox para opciones específicas
-    smoke = st.selectbox("¿Fumas?", ["No", "Ocasionalmente", "Frecuentemente", "Sí"])
-    calc = st.selectbox("Consumo de alcohol", ["Nunca", "A veces", "Frecuentemente", "Diario"])
-    mtrans = st.selectbox("Medio de transporte", ["Automóvil", "Bicicleta", "Motocicleta", "Transporte público", "A pie"])
+    calc = st.selectbox("Frecuencia consumo de alcohol", ["Nunca", "A veces", "Frecuentemente", "A diario"])
+    mtrans = st.selectbox("Medio de transporte que más uses", ["Automóvil", "Bicicleta", "Motocicleta", "Transporte público", "A pie"])
     
     # Otros valores numéricos
     favc = st.slider("Consumo de comida hipercalórica (1-5)", 1, 5, 3)
     fcvc = st.slider("Consumo de vegetales (1-5)", 1, 5, 3)
-    ncp = st.slider("Número de comidas al día", 1, 10, 3)
-    faf = st.slider("Actividad física por semana (horas)", 0, 20, 3)
+    ncp = st.slider("Número de comidas al día", 1, 6, 3)
+    faf = st.slider("Actividad física por semana (0-3)", 0, 3, 3)
     
-    # Cálculo de IMC
-    if altura > 0:
-        imc = peso / ((altura / 100) ** 2)
+    # Checkbox
+    smoke = st.checkbox("¿Fumas?")
+    historial_familiar = st.checkbox("Historial Familiar de obesidad")
     
-    print(f"Edad {edad}")
-    print(f"Altura {altura}")
-    print(f"Peso  {peso}")
-    print(f"Antecedente familiar  {historial_familiar}")
-    print(f"Fuma {smoke}")
-    print(f"Alcohol {calc}")
-    print(f"Transporte {mtrans}")
-    print(f"Consumo comida hipercalorica {favc}")
-    print(f"Consumo vegetales {fcvc}")
-    print(f"Comidas del día {ncp}")
-    print(f"Actividad física {faf}")
+    # Diccionario con los datos
+    
     
     # Botón de envío
     if st.button("Enviar"):
         st.success("Datos enviados correctamente")
-        datos = st.json({
+        # Cálculo de IMC
+        if altura > 0:
+            imc = peso / (altura ** 2)
+        #Recoger datos
+        # Muestra los datos en formato JSON
+        datos = {
             "Age": edad,
             "Height": round(altura, 2),
             "Weight": round(peso, 2),
@@ -63,15 +55,14 @@ def main():
             "CALC": calc,
             "MTRANS": mtrans,
             "IMC": round(imc, 2)
-        })
-        
-        df = pd.DataFrame([datos])
-        
+            } 
+        # Convertir el diccionario a un DataFrame de pandas
+        df = pd.DataFrame([datos])  
         #Preprocesamiento
-        df = pre.preprocess()
-         # Llamar al modelo y obtener la respuesta
-        resultado = model(df)
-        
+        df = pre.preprocess(df)
+        # Llamar al modelo y obtener la respuesta
+        predict = model(df)
+        resultado = pre.postptocess(predict)
         # Mostrar la predicción en la app
         st.success(resultado)
 
--- preprocessing.py
+++ preprocessing.py
@@ -14,10 +14,27 @@ class Process:
             
         x[["FCVC","NCP","FAF"]]= x[["FCVC","NCP","FAF"]].astype(int)
 
-        x['CALC'] = x['CALC'].replace({'no': 0, 'Sometimes': 1, 'Frequently': 2, 'Always': 3}).astype(int)
+        x['CALC'] = x['CALC'].replace({'Nunca': 0, 'A veces': 1, 'Frecuentemente': 2, 'A diario': 3}).astype(int)
         
-        x['MTRANS'] = x['MTRANS'].replace({'Walking': 0, 'Bike': 1, 'Motorbike': 2, 'Automobile': 3, 'Public_Transportation':4}).astype(int)
+        x['MTRANS'] = x['MTRANS'].replace({'A pie': 0, 'Bicicleta': 1, 'Motocicleta': 2, 'Automóvil': 3, 'Transporte público':4}).astype(int)
         
-        x['NObeyesdad'] = x['NObeyesdad'].replace({'Insufficient_Weight': 0, 'Normal_Weight': 1, 'Overweight_Level_I': 2, 'Overweight_Level_II': 3, 'Obesity_Type_I':4, 'Obesity_Type_II':5, 'Obesity_Type_III':6}).astype(int)
+        #x['NObeyesdad'] = x['NObeyesdad'].replace({'Insufficient_Weight': 0, 'Normal_Weight': 1, 'Overweight_Level_I': 2, 'Overweight_Level_II': 3, 'Obesity_Type_I':4, 'Obesity_Type_II':5, 'Obesity_Type_III':6}).astype(int)
         
         return x
+    
+    
+    def postptocess(prediccion):
+        clases = {
+            0 :'Peso insuficiente, estás por debajo de la media',
+            1 : 'Peso normal sigue así',
+            2 : 'Estás un poco por encima del peso, pero es saludable',
+            3 : 'Estás por encima del peso y entrando en una zona peligrosa para la salud',
+            4 : 'Obesidad de tipo 1 a darle zapatilla, hay que bajar de peso',
+            5 : 'Obesodad de tipo 2',
+            6 : 'Obesidad de tipo 3'
+        }
+        
+        valores = prediccion.numpy()
+        clase_idx = valores.argmax()
+        
+        return clases.get(clase_idx)
